fe018780f90d07aaff18565919d5e5f8e82adc3e
Sergei Basharkin <Basharkin@lanit.ru>
2023-08-07 17:47:06 +0300

    Initial commit

---
+++ src/test/java/com/example/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demojenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+  @Test
+  void contextLoads() {
+    Assertions.assertEquals(1,1);
+  }
+
+}
